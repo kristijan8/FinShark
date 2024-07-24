621944e3050f5a49b88d27ae5aabe5d05582ca41
Kristijan Miloseski <kristijan.miloseski.f@gmail.com>
2024-07-24 16:38:59 +0200

    bazaa

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e31db404326c49fef9024dda29718ee490a1e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f56b240ebefaf338f484a59895542521ad2764")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
