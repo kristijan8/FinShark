4cc4b25a303b34e6da76cb4044c93c35b8539f9a
Kristijan Miloseski <kristijan.miloseski.f@gmail.com>
2024-07-24 15:23:45 +0200

    asd

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -13,8 +13,6 @@ namespace api.Models
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public int? StockId { get; set; }
         public Stock? Stock {get; set;}
-        public int a;
-        public int c;
 
     }
 }
