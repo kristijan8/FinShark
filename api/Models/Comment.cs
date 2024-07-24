77a327f03f5bf0be3e64eb487eff8bc3eaaf6c27
Kristijan Miloseski <kristijan.miloseski.f@gmail.com>
2024-07-24 15:21:20 +0200

    hyg

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -13,6 +13,7 @@ namespace api.Models
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public int? StockId { get; set; }
         public Stock? Stock {get; set;}
+        public int b;
 
     }
 }
