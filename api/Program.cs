62d60ee888eb42f276bd97c3e6eeb42b61a2c077
Kristijan Miloseski <kristijan.miloseski.f@gmail.com>
2024-07-28 14:39:49 +0200

    wqd

--- api/Program.cs
+++ api/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Sve do identity
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
